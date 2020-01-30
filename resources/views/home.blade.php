f9f1840bd0d724d5b8bfd0db3b2eb75b86f70d07
SilviaYembi <s.mik@hotmail.com>
2020-01-31 10:07:02 +1100

    Add new blog post button added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,14 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-        <h2 class="text-center mb-5">My blog posts </h2>
+            <h2 class="text-center mb-5">My blog posts </h2>
 
             <div class="card">
-
+                <div class="text-center mt-3">
+                    <a href="{{ url('new-post') }}" class="btn btn-success"> Add new </a>
+                </div>
                 <div class="card-body">
-                
+
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
@@ -21,17 +23,17 @@
                         @foreach ($my_blog_posts as $post)
 
                         <li class="list-group-item">
-                            <h4> {{ $loop->iteration }}.  {{ $post->title }}</h4>
+                            <h4> {{ $loop->iteration }}. {{ $post->title }}</h4>
                             <p class="font-italic"> {{ $post->content }} </p>
-
-                            <a href="{{ url('delete-post/' . $post->post_id ) }}" onclick="return confirm('Are you sure?')"  class="btn btn-danger" role="button" title="Delete this post"> Delete </a>
+                            <p class="font-weight-bold float-right"> Created at {{ date('d-M-y H:i', strtotime($post->created_at)) }} </p>
+                            <a href="{{ url('delete-post/' . $post->post_id ) }}" onclick="return confirm('Are you sure?')" class="btn btn-sm btn-danger mt-3" role="button" title="Delete this post"> Delete </a>
                         </li>
                         @endforeach
                     </ul>
                     @else
                     <div class="text-center">
                         <h4> I don't have any post!</h4>
-                        <a href="{{ url('new-post') }}" class="btn btn-success" > Create post </a>
+                        <a href="{{ url('new-post') }}" class="btn btn-success"> Create post </a>
                     </div>
                     @endif
                 </div>
